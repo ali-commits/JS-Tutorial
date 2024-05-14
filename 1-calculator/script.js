d6317ae9a06ece42dc24a661a3be19f09f0218ec
Ali Alrabeei <newaaa4@gmail.com>
2024-05-14 13:15:34 +0800

    update the js file after class 1

--- 1-calculator/script.js
+++ 1-calculator/script.js
@@ -12,16 +12,46 @@ document.getElementById('2').addEventListener('click', function () {
     output.innerHTML = value;
 });
 
+document.getElementById('3').addEventListener('click', function () {
+    value += '3';
+    output.innerHTML = value;
+});
+
+document.getElementById('4').addEventListener('click', function () {
+    value += '4';
+    output.innerHTML = value;
+});
+
+document.getElementById('5').addEventListener('click', function () {
+    value += '5';
+    output.innerHTML = value;
+});
+
+document.getElementById('6').addEventListener('click', function () {
+    value += '6';
+    output.innerHTML = value;
+});
+
+document.getElementById('7').addEventListener('click', function () {
+    value += '7';
+    output.innerHTML = value;
+});
+
+document.getElementById('8').addEventListener('click', function () {
+    value += '8';
+    output.innerHTML = value;
+});
+
+document.getElementById('9').addEventListener('click', function () {
+    value += '9';
+    output.innerHTML = value;
+});
+
+document.getElementById('0').addEventListener('click', function () {
+    value += '0';
+    output.innerHTML = value;
+});
+
 document.getElementById('clear').addEventListener('click', function () {
     output.innerHTML = '';
-});
-
-document.getElementById('=').addEventListener('click', function () {
-    document.getElementById('output').style.backgroundColor = 'red';
-    
-});
-
-document.getElementById('backspace').addEventListener('click', function () {
-    value = value.slice(0, -1);
-    output.innerHTML = value;
 });
