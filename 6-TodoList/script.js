7a9538c95bff6428c12e524a0ab3739aae19eda5
ali-commits <newaaa4@gmail.com>
2024-06-07 01:03:10 +0800

    add the requirements

---
+++ 6-TodoList/script.js
@@ -0,0 +1,10 @@
+// Requirements
+// 1. Add a new task to the list when the user clicks the "Add" button.
+// 2. Add a checkmark to a task when the user presses the "Enter" key on the keyboard.
+// 3. Remove a task from the list when the user clicks the "Remove" button.
+// 4. Change the background color of a task when the user clicks it and add a strikethrough line to the text. (marked as completed)
+// 5. Change the background color of a task back to white when the user clicks it again and remove the strikethrough line from the text. (marked as incomplete)
+// 6. Reorder the tasks in the list by dragging and dropping them.
+// 7. Add a "Clear All" button that removes all tasks from the list.
+
+// Solution
