e39b7ca4971de1297998072efc5593c19a8a07ed
Ali Alrabeei <newaaa4@gmail.com>
2024-06-08 23:48:22 +0800

    update the code for lesson 7

--- 7-TodoList/script.js
+++ 7-TodoList/script.js
@@ -1,15 +1,40 @@
-// Requirements
+function addTask() {
+    let inputValue = $('#myInput').val();
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        let task = $('<li></li>').html(
+            '<span class="task">' + inputValue + '</span>'
+        );
+        let closeBtn = $('<span></span>').addClass('close').text('🗑');
+        task.append(closeBtn);
+        $('#Todo').append(task);
+        $('#myInput').val('');
+    }
+}
+
 // 1. Add a new task to the list when the user clicks the "Add" button.
+$('.addBtn').click(addTask);
+
 // 2. Add a checkmark to a task when the user presses the "Enter" key on the keyboard.
+$('#myInput').keypress(function (e) {
+    if (e.which === 13) {
+        addTask();
+    }
+});
+
 // 3. Remove a task from the list when the user clicks the "Remove" button.
+$('.close').click(function () {
+    $(this).parent().hide();
+});
+
 // 4. Change the background color of a task when the user clicks it and add a strikethrough line to the text. (marked as completed)
 // 5. Change the background color of a task back to white when the user clicks it again and remove the strikethrough line from the text. (marked as incomplete)
+$('#Todo li').on('click', function () {
+    $(this).toggleClass('checked');
+});
 
 // 6. Reorder the tasks in the list by dragging and dropping them.
 
 // Homework
 // 7. Add a "Clear All" button that removes all tasks from the list.
-
-// Solution
-
-
