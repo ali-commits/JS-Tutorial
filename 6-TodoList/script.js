2cc34790dc25a13e5b3672e92fcc599615f14189
ali-commits <newaaa4@gmail.com>
2024-06-09 23:38:53 +0800

    update home work

--- 6-TodoList/script.js
+++ 6-TodoList/script.js
@@ -4,7 +4,10 @@
 // 3. Remove a task from the list when the user clicks the "Remove" button.
 // 4. Change the background color of a task when the user clicks it and add a strikethrough line to the text. (marked as completed)
 // 5. Change the background color of a task back to white when the user clicks it again and remove the strikethrough line from the text. (marked as incomplete)
+
 // 6. Reorder the tasks in the list by dragging and dropping them.
+
+// Homework
 // 7. Add a "Clear All" button that removes all tasks from the list.
 
 // Solution
