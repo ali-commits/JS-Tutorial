f34a2267a43cd55f4d9590657a92c436307c9be2
Ali Alrabeei <newaaa4@gmail.com>
2024-06-12 18:31:57 +0800

    update

--- weather/script.js
+++ weather/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-    const apiKey = 'key'; 
+    const apiKey = '';
 
     $('#search-button').on('click', function () {
         const city = $('#city').val();
         if (city) {
+            $('#weather-result').hide();
+            $('#loading').show();
             getWeather(city);
-            console.log(city)
         }
     });
 
@@ -15,12 +16,14 @@ $(document).ready(function () {
             displayWeather(data);
         }).fail(function () {
             alert('City not found. Please try again.');
+            $('#loading').hide();
         });
     }
 
     function displayWeather(data) {
         if (data.success === false) {
             alert('City not found. Please try again.');
+            $('#loading').hide();
             return;
         }
         $('#city-name').text(data.location.name);
@@ -28,6 +31,31 @@ $(document).ready(function () {
         $('#weather-description').text(
             `Weather: ${data.current.weather_descriptions[0]}`
         );
+
+        // Map weather descriptions to icons
+        const iconClass = getWeatherIcon(data.current.weather_descriptions[0]);
+        $('#weather-icon').attr('class', `wi ${iconClass}`);
+
+        $('#loading').hide();
         $('#weather-result').show();
+    }
+
+    function getWeatherIcon(description) {
+        switch (description.toLowerCase()) {
+            case 'clear':
+                return 'wi-day-sunny';
+            case 'partly cloudy':
+                return 'wi-day-cloudy';
+            case 'cloudy':
+                return 'wi-cloudy';
+            case 'rain':
+                return 'wi-rain';
+            case 'thunderstorm':
+                return 'wi-thunderstorm';
+            case 'snow':
+                return 'wi-snow';
+            default:
+                return 'wi-day-sunny';
+        }
     }
 });
